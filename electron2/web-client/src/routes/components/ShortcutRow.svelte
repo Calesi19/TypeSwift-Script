b877157e3c4b10636592d984a72f206d2b9a193f
Carlos Lespin <carlos.lespin.silva@gmail.com>
2023-11-30 01:33:05 -0500

    feat: react electron added

---
+++ electron2/web-client/src/routes/components/ShortcutRow.svelte
@@ -0,0 +1,34 @@
+<script>
+    // Export component
+
+	import { Button } from 'flowbite-svelte';
+	import {
+		Input,
+		TableBodyCell,
+		TableBodyRow,
+	} from 'flowbite-svelte';
+	import { Textarea } from 'flowbite-svelte';
+	import { TrashBinSolid } from 'flowbite-svelte-icons';
+	let textareaprops = {
+		id: 'message',
+		name: 'message',
+		label: 'Your text',
+		rows: 4,
+		placeholder: 'Type your text here...'
+	};
+
+</script>
+
+<TableBodyRow>
+    <TableBodyCell><Input id="default-input" placeholder="shorthand" /></TableBodyCell>
+    <TableBodyCell>
+        <Textarea {...textareaprops} />
+    </TableBodyCell>
+    <TableBodyCell>
+        <Button color="light">
+            <TrashBinSolid class="w-5 h-5 text-slate-500 hover:text-red-700" />
+        </Button>
+    </TableBodyCell>
+</TableBodyRow>
+
+
---
+++ web-client/src/routes/components/firebase.svelte
@@ -0,0 +1,25 @@
+<script>
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD_agqP6rC8ZU2aCOdr4ZZdECppba55soE",
+  authDomain: "typeswift-3af2c.firebaseapp.com",
+  projectId: "typeswift-3af2c",
+  storageBucket: "typeswift-3af2c.appspot.com",
+  messagingSenderId: "601167723759",
+  appId: "1:601167723759:web:89a4d542c62ef637856f1a",
+  measurementId: "G-5KZSQRY2CJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+</script>
